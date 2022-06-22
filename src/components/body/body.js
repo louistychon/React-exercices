fbc3ff9a79bec61de9518a407bfaf5a8ec9c4122
louis.tychon1@gmail.com <louis.tychon1@gmail.com>
2022-06-22 11:56:35 +0200

    first props

--- src/components/body/body.js
+++ src/components/body/body.js
@@ -2,23 +2,22 @@ import React from 'react';
 import "./body.css"
 import {Article} from '../article/article.js'
 
-let nombreArticles = prompt("entrez un nombre d'articles")
 function nombreArticle(Articles){
-if(nombreArticles >= 2){
-    return (<div className="container">
-    <Article/>
-    <Article/>
-</div>)  
-}
-else{
-    return (<h1>Il n'y a pas d'articles</h1>)
-}
+    if(Articles >= 2){
+        return (<div className="container">
+        <Article titre="Les tartines" />
+        <Article titre="Coding school"/>
+    </div>)  
+    }
+    else{
+        return (<h1>Il n'y a pas d'articles</h1>)
+    }
 }
 class Body extends React.Component{
     render(){
         return(
             <section id="body">
-               {nombreArticle(1)}
+               {nombreArticle(2)}
             </section>
         )
     }
